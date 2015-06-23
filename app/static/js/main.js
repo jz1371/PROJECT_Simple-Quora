4da58855c954a775c153163b569bb545790fa0bf
Jingxin <jz1371@nyu.edu>
2015-06-23 17:29:01 -0400

    disable animation on flask message

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -7,9 +7,9 @@
  */
 
 /* let '#flash_message' fade out and then below div comes up */
-$(document).ready(function() {
-    $('#flash_message').delay(2000).animate({opacity: 0}, 1000, "linear", function() {
-        $('#flash_message').slideUp("slow");
-    });
-});
+//$(document).ready(function() {
+//    $('#flash_message').delay(2000).animate({opacity: 0}, 1000, "linear", function() {
+//        $('#flash_message').slideUp("slow");
+//    });
+//});
 
